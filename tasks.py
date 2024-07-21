bc1df25d422c49a028ef505cd409d17d21862361
runtime-error786 <mustafa782a@gmail.com>
2024-07-21 19:56:08 +0500

    complete

--- tasks.py
+++ tasks.py
@@ -3,17 +3,17 @@ from agents import researcher,writer
 
 
 task1 = Task(
-    description="""Research and find 5 promising investment properties in {country}. 
-                   Include the name of the city, country, location, measurement, and profit or loss ratio for each property.""",
+    description="""Research and find 3 promising investment properties in {country}. 
+                   Include the name of the property, city, country, location for each property.""",
     expected_output="""A detailed list of five properties with the following format:
     
     Property 1:
+    name : [property name]
     City: [City Name]
     Country: Australia
     Location: [Location Details]
-    Measurement: [Measurement Details]
-    Profit or Loss Ratio: [Ratio]
-    ... and so on for 5 properties.""",
+
+    ... and so on for 3 properties.""",
     agent=researcher
 )
 
@@ -22,12 +22,10 @@ task2 = Task(
     description="Summarize the property information into a bullet point list for each property in comprehensive.",
     expected_output="""A summarized list of each property in comprehensive including :
     
-    Property 1:
+    name : [property name]
     City: [City Name]
     Country: Australia
     Location: [Location Details]
-    Measurement: [Measurement Details]
-    Profit or Loss Ratio: [Ratio]
     
     and so on...""",
     agent=writer,
